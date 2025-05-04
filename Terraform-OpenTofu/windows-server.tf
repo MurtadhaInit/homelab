3ad1a455383eed61d2140c3ed9b3f9ad539dd009
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-05-04 03:26:03 +0300

    Simple rename + added a few comments in windows-server.tf

--- Terraform-OpenTofu/windows-server.tf
+++ Terraform-OpenTofu/windows-server.tf
@@ -63,12 +63,13 @@ resource "proxmox_virtual_environment_vm" "windows-server" {
   }
   disk {
     datastore_id = "local-lvm"
-    interface    = "scsi0"
-    discard      = "on"
-    iothread     = true
-    ssd          = true
-    size         = 200
-    cache        = "writeback"
+    # interface = "virtio" # maybe this is better, and let the virtio drivers handle the rest?
+    interface = "scsi0"
+    discard   = "on"
+    iothread  = true
+    ssd       = true
+    size      = 200
+    cache     = "writeback"
   }
   cdrom {
     file_id = proxmox_virtual_environment_download_file.windows_server_iso.id
@@ -102,5 +103,9 @@ variable "windows_server_static_ip" {
 # 1. Before starting the VM, mount a 2nd CD ROM with the virtio drivers (in v1 of the provider, it'll be possible to mount two CDs)
 # 2. During installation load the drivers in the vioscsi folder to recognise the disk
 # 3. After installation, install the qemu guest agent (guest-agent directory) and drivers for unrecognised devices (point to the virtio drivers CD to search for drivers). Without those drivers there might not be any internet access.
+# - Or... install everything through the installer at the root of the virtio drivers disk
 # 4. Activate windows: irm https://get.activated.win | iex
 # 5. Enable remote desktop
+# 6. Install Windows updates then reserve the IP address of 10.20.30.42 for the machine in the router before restarting (restarting will make the machine release the old IP)
+# 7. Use winutil: `irm "https://christitus.com/win" | iex` to load the exported settings file. Install apps and apply teaks.
+# As Admin: iex "& { $(irm https://christitus.com/win) } -Config [path-to-your-config] -Run"
